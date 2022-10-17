307ed6fe74f4b774304504b9f9ab05ebc4d6f66f
jingying20 <jingyingliangdesign@gmail.com>
2022-10-16 22:38:10 -0700

    Preparing main index

--- src/js/signin.js
+++ src/js/signin.js
@@ -3,28 +3,30 @@ const checkSigninForm = () => {
     const userval = $("#signin-username").val();
     const passval = $("#signin-password").val();
 
-    console.log(userval,passval)
+    console.log(userval, passval)
 
     if (userval === "user" && passval === "pass") {
-        //Logged In 
+        // Logged In
         console.log("Success");
         sessionStorage.userId = 3;
 
+        $("#signin-form")[0].reset();
     } else {
-        //Not Logged
+        // Not Logged In
         console.log("Failure");
         sessionStorage.removeItem("userId");
 
-        $(".warning").html();
+        $(".warning").html("");
         setTimeout(()=>{$(".warning").html("");},3000)
     }
 
     checkUserId();
 }
 
+
 const checkUserId = () => {
-    const pages = ["#signin-page", "#signup-page",""];
-    
+    const pages = ["#signin-page", "#signup-page", ""];
+
     if (sessionStorage.userId === undefined) {
         // not logged in
         if (!pages.some(p => p === window.location.hash)) {
