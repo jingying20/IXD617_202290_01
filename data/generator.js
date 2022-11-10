e0d4c0c01c283f4872079ef561c7c09e3e9a9d15
jingying20 <jingyingliangdesign@gmail.com>
2022-11-09 18:16:49 -0800

    added data generator

---
+++ data/generator.js
@@ -0,0 +1,83 @@
+
+const hex = () => chance.string({length:3, pool:'3456789AB'});
+const num = () => chance.natural({min:350,max:600});
+const getdate = (d1,d2) => dayjs(new Date(chance.natural({min:d1,max:d2}))).format('YYYY-MM-DD HH:mm:ss');
+
+const benchtype = (type) => {
+    let types = {
+        'wood':['log-color','light-color','dark-color','colorful','contemporary-design'],
+        'stone':['light-color','dark-color','colorful','contemporary-design'],
+        'steel':['light-color','dark-color','colorful','contemporary-design']
+    }
+    return chance.pickone(types[type]);
+}
+
+const getUsers = () => (new Array(10)).fill(0).map((o,i)=>{
+    o = {};
+    o.id = i + 1;
+    o.name = chance.name();
+    o.username = 'user'+o.id;
+    o.password = md5('pass');
+    o.email = o.username+'@gmail.com';
+    o.img = `https://via.placeholder.com/${num()}x${num()}/${hex()}/fff/?text=${o.name}`;
+    o.date_create = getdate(Date.parse('2020/01/01'),Date.now());
+    return o;
+});
+
+const getBenches = () => (new Array(50)).fill(0).map((o,i)=>{
+    o = {};
+    o.id = i + 1;
+    o.user_id = chance.natural({min:1, max:10});
+    o.name = chance.first();
+    o.type = chance.pickone(['wood','stone','steel']);
+    o.style = benchtype(o.type);
+    o.description = chance.sentence();
+    o.img = `https://via.placeholder.com/${num()}x${num()}/${hex()}/fff/?text=${o.name}`;
+    o.date_create = getdate(Date.parse('2020/01/01'),Date.now());
+    return o;
+});
+
+const getLocations = () => (new Array(250)).fill(0).map((o,i)=>{
+    o = {};
+    o.id = i + 1;
+    o.bench_id = chance.natural({min:1, max:50});
+    o.lat = chance.latitude({min:37.08, max:37.81});
+    o.lng = chance.longitude({min:-122.47, max:-121.74});
+    o.description = chance.sentence();
+    o.photo = `https://via.placeholder.com/${num()}x${num()}/${hex()}/fff/?text=PHOTO`;
+    o.icon = `https://via.placeholder.com/${num()}x${num()}/${hex()}/fff/?text=ICON`;
+    o.date_create = getdate(Date.parse('2020/01/01'),Date.now());
+    return o;
+});
+
+
+
+const exportAsJSON = (type,data) => {
+    document.querySelector('.output').innerHTML = `
+<h2>${type}</h2>
+<pre>
+[
+${data.map((o)=>`${JSON.stringify(o)}`).join(',\n')}
+]
+</pre>
+`;
+}
+
+const exportAsSQL = (type,data) => {
+    document.querySelector('.output').innerHTML = `
+<h2>${type}</h2>
+<pre>
+INSERT INTO \`${type}\` (\`${Object.keys(data[0]).join('`, `')}\`) VALUES
+${data.map((o)=>`(${JSON.stringify(Object.values(o)).slice(1,-1)})`).join(',\n')}
+</pre>
+`;
+}
+
+window.addEventListener('DOMContentLoaded',()=>{
+    document.querySelector('.users-json').addEventListener('click',()=>{ exportAsJSON('Users',getUsers()); });
+    document.querySelector('.benches-json').addEventListener('click',()=>{ exportAsJSON('Benches',getBenches()); });
+    document.querySelector('.locations-json').addEventListener('click',()=>{ exportAsJSON('Locations',getLocations()); });
+    document.querySelector('.users-sql').addEventListener('click',()=>{ exportAsSQL('track_users',getUsers()); });
+    document.querySelector('.benches-sql').addEventListener('click',()=>{ exportAsSQL('track_benches',getBenches()); });
+    document.querySelector('.locations-sql').addEventListener('click',()=>{ exportAsSQL('track_locations',getLocations()); });
+});
